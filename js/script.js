39b8f6b016481494c1c08fcc5328d8b11ad807c0
DOULORES <lucas.carvalho30111@gmail.com>
2025-09-22 20:25:49 -0300

    Ajusta colocando validação para o usuário não passar um dos campos

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ function diferenca() {
     let valor1 = document.getElementById("valor1").value;
     let valor2 = document.getElementById("valor2").value;
 
+    if (!valor1 || !valor2) {
+        console.log("entrou aqui");
+        alert("Por favor, digite valores numéricos válidos em ambos os campos.");
+        return;
+    }
+
     resultado = "Os números são iguais";
     if (parseInt(valor1) != parseInt(valor2)) {
         resultado = "Os números são diferentes";
